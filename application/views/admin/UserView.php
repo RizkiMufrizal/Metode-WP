7f398dd90a076527462cc32d719f916321027c5a
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-11 18:52:19 +0700

    update user view

--- application/views/admin/UserView.php
+++ application/views/admin/UserView.php
@@ -14,7 +14,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>Metode SAW</title>
+        <title>Metode WP</title>
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -29,7 +29,7 @@
     </head>
     <body>
 
-        <nav class="navbar navbar-default">
+        <nav class="navbar navbar-inverse">
             <div class="container-fluid">
 
                 <div class="navbar-header">
@@ -38,7 +38,7 @@
                         <span class="icon-bar"></span> <span class="icon-bar"></span> 
                         <span class="icon-bar"></span> 
                     </button>
-                    <a class="navbar-brand" href="#">Metode SAW</a>
+                    <a class="navbar-brand" href="#">Metode WP</a>
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
@@ -52,31 +52,40 @@
                 </div>
             </div>
         </nav>
-        
+
         <div class="container">
 
-            <form class="form-signin" method="post" action="<?php echo base_url(); ?>index.php/admin/login">
-
-                <?php if ($error != null) { ?>
-                    <div class="alert alert-danger alert-dismissible" role="alert">
-                        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                        <strong>Warning !</strong> <?php echo $error; ?>
-                    </div>
-                <?php } ?>
-
-                <h2 class="form-signin-heading">Please sign in</h2>
-
-                <label for="inputEmail" class="sr-only">Email address</label>
-                <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-                <label for="inputPassword" class="sr-only">Password</label>
-                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                <div class="checkbox">
-                    <label>
-                        <input type="checkbox" value="remember-me"> Remember me
-                    </label>
+            <div class="panel panel-default form-signin">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Please sign in</h3>
                 </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-            </form>
+                <div class="panel-body">
+                    <form accept-charset="UTF-8" role="form" method="post" action="<?php echo base_url(); ?>index.php/admin/login">
+
+                        <?php if ($error != null) { ?>
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                <strong>Warning !</strong> <?php echo $error; ?>
+                            </div>
+                        <?php } ?>
+
+                        <fieldset>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="E-mail" name="email" type="text">
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                            </div>
+                            <div class="checkbox">
+                                <label>
+                                    <input name="remember" type="checkbox" value="Remember Me"> Remember Me
+                                </label>
+                            </div>
+                            <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
 
         </div>
 
