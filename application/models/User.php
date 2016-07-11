2d440fc9bc54c166ffec273b6e6ccbea41b86fd5
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-11 16:49:51 +0700

    update model user

--- application/models/User.php
+++ application/models/User.php
@@ -1,20 +1,21 @@
 <?php
 
 /**
- * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
- * Since May 4, 2016
- * Time 5:15:57 PM
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com> <https://RizkiMufrizal.github.io>
+ * Since Jul 11, 2016
+ * Time 4:48:16 PM
  * Encoding UTF-8
- * Project Metode-SAW
+ * Project Metode-WP
  * Package Expression package is undefined on line 13, column 14 in Templates/Scripting/PHPClass.php.
+ *
  */
-class User extends CI_Model
-{
-    public function ambilUserUntukLogin($email)
-    {
-        $this->db->where('email', $email);
-        $this->db->select('password');
+class User extends CI_Model {
 
+    public function ambilUserUntukLogin($username) {
+        $this->db->where('username', $username);
+        $this->db->select('password');
         return $this->db->get('tb_user')->result();
     }
+
 }
