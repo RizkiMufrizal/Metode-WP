3bbca0dff58aac5807a42c0b976c0d0543c4f3bb
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-11 16:50:22 +0700

    update upload csv

--- application/controllers/admin/CalonSiswaController.php
+++ application/controllers/admin/CalonSiswaController.php
@@ -1,12 +1,14 @@
 <?php
 
 /**
- * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
- * Since Apr 21, 2016
- * Time 7:20:48 PM
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com> <https://RizkiMufrizal.github.io>
+ * Since Jul 11, 2016
+ * Time 4:48:16 PM
  * Encoding UTF-8
- * Project Metode-Saw
- * Package Expression package is undefined on line 14, column 14 in Templates/Scripting/PHPClass.php.
+ * Project Metode-WP
+ * Package Expression package is undefined on line 13, column 14 in Templates/Scripting/PHPClass.php.
+ *
  */
 class CalonSiswaController extends CI_Controller {
 
@@ -15,6 +17,7 @@ class CalonSiswaController extends CI_Controller {
         $this->load->library('CSVReader');
         $this->load->model('CalonSiswa');
         $this->load->model('NilaiCalonSiswa');
+        $this->load->model('Himpunan');
         $this->load->model('Kriteria');
     }
 
@@ -33,11 +36,12 @@ class CalonSiswaController extends CI_Controller {
         $val = array(
             'nisn' => $this->input->post('nisn'),
             'nama' => $this->input->post('nama'),
-            'jenis_kelamin' => $this->input->post('jenis_kelamin'),
+            'tempat_lahir' => $this->input->post('tempat_lahir'),
             'tanggal_lahir' => $this->input->post('tanggal_lahir'),
-            'alamat' => $this->input->post('alamat'),
-            'asal_sekolah' => $this->input->post('asal_sekolah'),
-            'tahun_lulus' => $this->input->post('tahun_lulus')
+            'nama_orang_tua' => $this->input->post('nama_orang_tua'),
+            'pekerjaan_orang_tua' => $this->input->post('pekerjaan_orang_tua'),
+            'no_telepon' => $this->input->post('no_telepon'),
+            'keterangan' => $this->input->post('keterangan')
         );
         $this->CalonSiswa->tambahCalonSiswa($val);
         redirect('admin/CalonSiswaController');
@@ -75,11 +79,12 @@ class CalonSiswaController extends CI_Controller {
                 $val = array(
                     'nisn' => $row['nisn'],
                     'nama' => $row['nama'],
-                    'jenis_kelamin' => $row['jenis_kelamin'],
+                    'tempat_lahir' => $row['tempat_lahir'],
                     'tanggal_lahir' => $row['tanggal_lahir'],
-                    'alamat' => $row['alamat'],
-                    'asal_sekolah' => $row['asal_sekolah'],
-                    'tahun_lulus' => $row['tahun_lulus']
+                    'nama_orang_tua' => $row['nama_orang_tua'],
+                    'pekerjaan_orang_tua' => $row['pekerjaan_orang_tua'],
+                    'no_telepon' => $row['no_telepon'],
+                    'keterangan' => $row['keterangan']
                 );
                 $this->CalonSiswa->tambahCalonSiswa($val);
             }
@@ -87,20 +92,47 @@ class CalonSiswaController extends CI_Controller {
             //simpan calon siswa
             foreach ($result as $row) {
                 $nisn = $row['nisn'];
-                $c1 = $row['nilai_psikotes'];
-                $c2 = $row['nilai_psm_test'];
-                $c3 = $row['nilai_angket_siswa'];
-                $c4 = $row['nilai_un'];
-                $c5 = $row['nilai_raport'];
+                $c1 = $row['nilai_peminatan'];
+                $c2 = $row['nilai_hasil_pendekatan_psikotest'];
+                $c3 = $row['nilai_placementtes'];
+                $c4 = $row['nilai_raport'];
+                $c5 = $row['nilai_un'];
+
+                foreach ($this->Himpunan->ambilHimpunan() as $h) {
+
+                    if ($c1 >= $h->batas_atas and $c1 <= $h->batas_bawah) {
+                        $c1 = $h->nilai;
+                    }
+
+                    if ($c2 >= $h->batas_atas and $c2 <= $h->batas_bawah) {
+                        $c2 = $h->nilai;
+                    }
+
+                    if ($c3 >= $h->batas_atas and $c3 <= $h->batas_bawah) {
+                        $c3 = $h->nilai;
+                    }
+
+                    if ($c4 >= $h->batas_atas and $c4 <= $h->batas_bawah) {
+                        $c4 = $h->nilai;
+                    }
+
+                    if ($c5 >= $h->batas_atas and $c5 <= $h->batas_bawah) {
+                        $c5 = $h->nilai;
+                    }
+                }
 
                 $val = array(
-                    'id_nilai' => $this->uuid->v4(),
+                    'nilai_asli_c1' => $row['nilai_peminatan'],
+                    'nilai_asli_c2' => $row['nilai_hasil_pendekatan_psikotest'],
+                    'nilai_asli_c3' => $row['nilai_placementtes'],
+                    'nilai_asli_c4' => $row['nilai_raport'],
+                    'nilai_asli_c5' => $row['nilai_un'],
                     'c1' => $c1,
                     'c2' => $c2,
                     'c3' => $c3,
                     'c4' => $c4,
                     'c5' => $c5,
-                    'nisn' => $nisn,
+                    'nisn' => $nisn
                 );
 
                 $this->NilaiCalonSiswa->tambahNilaiCalonSiswa($val);
