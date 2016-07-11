d002bc3baa4fc8bdeef219466e4f2eab68fddf6b
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-11 18:52:09 +0700

    update index view

--- application/views/IndexView.php
+++ application/views/IndexView.php
@@ -1,25 +1,23 @@
 <!DOCTYPE html>
 <!--
-
-    @Author Rizki Mufrizal <mufrizalrizki@gmail.com>
-    @Since May 29, 2016
-    @Time 1:59:11 AM
+    @Author Rizki Mufrizal <mufrizalrizki@gmail.com> <https://RizkiMufrizal.github.io>
+    @Since Jul 11, 2016
+    @Time 3:24:09 PM
     @Encoding UTF-8
-    @Project Metode-SAW
-    @Package Expression package is undefined on line 9, column 16 in Templates/Scripting/EmptyPHPWebPage.php.
-
+    @Project Metode-WP
+    @Package Expression package is undefined on line 8, column 16 in Templates/Scripting/EmptyPHPWebPage.php.
 -->
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Metode SAW</title>
+        <title>Metode WP</title>
 
         <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
         <link href="<?php echo base_url(); ?>assets/css/dataTables.bootstrap.min.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
 
-        <nav class="navbar navbar-default">
+        <nav class="navbar navbar-inverse">
             <div class="container-fluid">
 
                 <div class="navbar-header">
@@ -28,7 +26,7 @@
                         <span class="icon-bar"></span> <span class="icon-bar"></span> 
                         <span class="icon-bar"></span> 
                     </button>
-                    <a class="navbar-brand" href="#">Metode SAW</a>
+                    <a class="navbar-brand" href="#">Metode WP</a>
                 </div>
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
@@ -50,17 +48,19 @@
                         <tr>
                             <th>NISN</th>
                             <th>Nama</th>
-                            <th>Nilai Psikotes</th>
-                            <th>Nilai PSM Test</th>
-                            <th>Nilai Angket Siswa</th>
-                            <th>Nilai UN</th>
+                            <th>Nilai Peminatan</th>
+                            <th>Nilai Hasil Pendekatan Psikotest</th>
+                            <th>Nilai Placementtes</th>
                             <th>Nilai Raport</th>
+                            <th>Nilai UN</th>
                             <th>Total Nilai</th>
                             <th>Rangking</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php $i = 0; foreach ($normalisasi as $n) { ++$i; ?>
+                        <?php $i = 0;
+                        foreach ($normalisasi as $n) {
+                            ++$i; ?>
                             <tr>
                                 <td><?php echo $n->nisn; ?></td>
                                 <td><?php echo $n->nama; ?></td>
