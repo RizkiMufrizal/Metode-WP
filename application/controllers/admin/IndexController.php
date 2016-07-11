bd3fd45719e7b7209e06485c8176388b064cfa54
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-11 16:50:54 +0700

    update index controller

--- application/controllers/admin/IndexController.php
+++ application/controllers/admin/IndexController.php
@@ -1,17 +1,18 @@
 <?php
 
 /**
- * Author Rizki Mufrizal <mufrizalrizki@gmail.com>
- * Since Apr 21, 2016
- * Time 7:08:43 PM
+ *
+ * Author Rizki Mufrizal <mufrizalrizki@gmail.com> <https://RizkiMufrizal.github.io>
+ * Since Jul 11, 2016
+ * Time 4:48:16 PM
  * Encoding UTF-8
- * Project Metode-Saw
- * Package Expression package is undefined on line 14, column 14 in Templates/Scripting/PHPClass.php.
+ * Project Metode-WP
+ * Package Expression package is undefined on line 13, column 14 in Templates/Scripting/PHPClass.php.
+ *
  */
-class IndexController extends CI_Controller
-{
-    public function index()
-    {
+class IndexController extends CI_Controller {
+
+    public function index() {
         $session = $this->session->userdata('isLogin');
 
         if ($session == false) {
@@ -20,4 +21,5 @@ class IndexController extends CI_Controller
             $this->load->view('admin/IndexView');
         }
     }
+
 }
