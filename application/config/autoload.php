868222c87b5419f74d78ba2b714deefc0225db87
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-07-11 16:48:01 +0700

    hapus uuid

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database', 'session', 'Uuid', 'bcrypt');
+$autoload['libraries'] = array('database', 'session', 'bcrypt');
 
 /*
 | -------------------------------------------------------------------
