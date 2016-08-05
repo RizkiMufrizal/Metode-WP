c80e5247a34d5e0ad5430364c79fa5718c2cd0af
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-08-05 17:05:55 +0700

    ganti ke metode wp

--- application/views/admin/CalonSiswaTambahNilaView.php
+++ application/views/admin/CalonSiswaTambahNilaView.php
@@ -12,7 +12,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>AdminLTE 2 | Dashboard</title>
+        <title>Metode WP</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
         <?php $this->load->view('admin/layout/CssLayout') ?>
 
